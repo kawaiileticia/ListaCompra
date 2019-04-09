b8136dff222f9cc44b56db0ebd9757d68a2b4182
Paula Bicca <paula.bicca@gmail.com>
2019-04-09 12:04:22 -0300

    Criação do Adadpter Produtos e inserção dos produtos(Sem teste)

--- app/src/main/java/com/asasfracas/ListaCompras/BancoProdutos.java
+++ app/src/main/java/com/asasfracas/ListaCompras/BancoProdutos.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class Banco extends SQLiteOpenHelper {
+public class BancoProdutos extends SQLiteOpenHelper {
 
     private static final String NOME_BANCO = "listas-produtos";
     private static final int VERSAO_BANCO = 1;
 
-    public Banco(Context context){
+    public BancoProdutos(Context context){
         super(context, NOME_BANCO, null, VERSAO_BANCO);
     }
 
